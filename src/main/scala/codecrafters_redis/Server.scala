ea34ce20254a71d10be8d0f348df0c9725adc07c
crunchyfrog <javaveryhot@protonmail.ch>
2024-10-15 11:13:26 +0200

    remove the comments

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -9,8 +9,8 @@ object Server {
 
     // Uncomment this to pass the first stage
     //
-    // val serverSocket = new ServerSocket()
-    // serverSocket.bind(new InetSocketAddress("localhost", 6379))
-    // val clientSocket = serverSocket.accept() // wait for client
+    val serverSocket = new ServerSocket()
+    serverSocket.bind(new InetSocketAddress("localhost", 6379))
+    val clientSocket = serverSocket.accept() // wait for client
   }
 }
