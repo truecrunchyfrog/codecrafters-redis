23c828e35828c472fac7f7bdde811da222871fc8
crunchyfrog <javaveryhot@protonmail.ch>
2024-10-18 09:25:30 +0200

    add PING command

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -1,16 +1,22 @@
 package codecrafters_redis
 
 import java.net.{InetSocketAddress, ServerSocket}
+import scala.annotation.tailrec
 
 object Server {
   def main(args: Array[String]): Unit = {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println("Logs from your program will appear here!")
-
-    // Uncomment this to pass the first stage
-    //
-    val serverSocket = new ServerSocket()
+    val serverSocket = new ServerSocket
     serverSocket.bind(new InetSocketAddress("localhost", 6379))
     val clientSocket = serverSocket.accept() // wait for client
+
+    @tailrec def stream(): Unit = {
+      clientSocket.getInputStream.readAllBytes() match {
+        case "*1\r\n$4\r\nPING\r\n" =>
+          clientSocket.getOutputStream.write("+PONG\r\n".toArray.map(_.toByte))
+      }
+      stream()
+    }
+
+    stream()
   }
 }
